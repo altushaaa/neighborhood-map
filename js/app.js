41aa990c137ae42a048ad2a42739425aa234b918
altushaaa <altushaaa@yahoo.com>
2016-09-25 08:27:30 +0500

    locations/LocModel separated from from map callback

--- js/app.js
+++ js/app.js
@@ -1,94 +1,94 @@
+/* ======= Model ======= */
+/*The Model includes the popular tourist destinations in Astana - the capital city of Kazakhstan*/
+var locations = [
+    {
+        title : 'Baiterek Tower',
+        position : {lat: 51.1283, lng: 71.4305},
+        marker: {},
+        infowindow: {}
+    },
+    {
+        title : 'Palace of Peace and Reconciliation',
+        position : {lat: 51.123135, lng: 71.463532},
+        marker: {},
+        infowindow: {}
+    },
+    {
+        title : 'Ak-Orda Presidential Palace',
+        position : {lat: 51.125836, lng: 71.446325},
+        marker: {},
+        infowindow: {}
+    },
+    {
+        title : 'Rixos President Hotel Astana',
+        position : {lat: 51.133501, lng: 71.421223},
+        marker: {},
+        infowindow: {}
+    },
+    {
+        title : 'Khan Shatyr Shopping & Entertainment Center',
+        position : {lat: 51.132539, lng: 71.403822},
+        marker: {},
+        infowindow: {}
+    },
+    {
+        title : 'Astana Expo 2017',
+        position : {lat: 51.08922, lng: 71.415963},
+        marker: {},
+        infowindow: {}
+    },
+    {
+        title : 'Astana Opera',
+        position : {lat: 51.135319, lng: 71.41065},
+        marker: {},
+        infowindow: {}
+    },
+    {
+        title : 'Astana Marriott Hotel',
+        position : {lat:  51.128741, lng: 71.407373},
+        marker: {},
+        infowindow: {}
+    },
+    {
+        title : 'Nazarbayev University',
+        position : {lat: 51.09053, lng: 71.398165},
+        marker: {},
+        infowindow: {}
+    },
+    {
+        title : 'Mega Astana Shopping & Entertainment Center',
+        position : {lat: 51.145503, lng: 71.413648},
+        marker: {},
+        infowindow: {}
+    },
+    {
+        title : 'Duman Aquarium and Entertainment Center',
+        position : {lat: 51.1469, lng: 71.4149},
+        marker: {},
+        infowindow: {}
+    },
+    {
+        title : 'Hazret Sultan Mosque',
+        position : {lat: 51.125488, lng: 71.471935},
+        marker: {},
+        infowindow: {}
+    },
+    {
+        title : 'Astana Arena Stadium',
+        position : {lat: 51.108224, lng: 71.402636},
+        marker: {},
+        infowindow: {}
+    }
+];
+
+var LocModel = function(data) {
+  this.title = data.title;
+  this.position = data.position;
+  this.marker = data.marker;
+  this.infowindow = data.infowindow;
+};
+
 function loadApp() {
-
-  /* ======= Model ======= */
-  /*The Model includes the popular tourist destinations in Astana - the capital city of Kazakhstan*/
-  var locations = [
-      {
-          title : 'Baiterek Tower',
-          position : {lat: 51.1283, lng: 71.4305},
-          marker: {},
-          infowindow: {}
-      },
-      {
-          title : 'Palace of Peace and Reconciliation',
-          position : {lat: 51.123135, lng: 71.463532},
-          marker: {},
-          infowindow: {}
-      },
-      {
-          title : 'Ak-Orda Presidential Palace',
-          position : {lat: 51.125836, lng: 71.446325},
-          marker: {},
-          infowindow: {}
-      },
-      {
-          title : 'Rixos President Hotel Astana',
-          position : {lat: 51.133501, lng: 71.421223},
-          marker: {},
-          infowindow: {}
-      },
-      {
-          title : 'Khan Shatyr Shopping & Entertainment Center',
-          position : {lat: 51.132539, lng: 71.403822},
-          marker: {},
-          infowindow: {}
-      },
-      {
-          title : 'Astana Expo 2017',
-          position : {lat: 51.08922, lng: 71.415963},
-          marker: {},
-          infowindow: {}
-      },
-      {
-          title : 'Astana Opera',
-          position : {lat: 51.135319, lng: 71.41065},
-          marker: {},
-          infowindow: {}
-      },
-      {
-          title : 'Astana Marriott Hotel',
-          position : {lat:  51.128741, lng: 71.407373},
-          marker: {},
-          infowindow: {}
-      },
-      {
-          title : 'Nazarbayev University',
-          position : {lat: 51.09053, lng: 71.398165},
-          marker: {},
-          infowindow: {}
-      },
-      {
-          title : 'Mega Astana Shopping & Entertainment Center',
-          position : {lat: 51.145503, lng: 71.413648},
-          marker: {},
-          infowindow: {}
-      },
-      {
-          title : 'Duman Aquarium and Entertainment Center',
-          position : {lat: 51.1469, lng: 71.4149},
-          marker: {},
-          infowindow: {}
-      },
-      {
-          title : 'Hazret Sultan Mosque',
-          position : {lat: 51.125488, lng: 71.471935},
-          marker: {},
-          infowindow: {}
-      },
-      {
-          title : 'Astana Arena Stadium',
-          position : {lat: 51.108224, lng: 71.402636},
-          marker: {},
-          infowindow: {}
-      }
-  ];
-
-  var LocModel = function(data) {
-    this.title = data.title;
-    this.position = data.position;
-    this.marker = data.marker;
-    this.infowindow = data.infowindow;
-  };
 
   /*======View/Setting up GoogleMap======*/
   var map;
